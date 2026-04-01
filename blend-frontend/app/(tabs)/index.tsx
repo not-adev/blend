57d9132d4a3c8b64139d88ea69e02109a9e98648
not-adev <rcduaru2718@gmail.com>
2026-04-01 22:24:30 +0530

    initialization of app

---
+++ blend-frontend/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-black bg-green-600">NativeWind Working 🚀</Text>
+    </View>
+   
+  );
+}
